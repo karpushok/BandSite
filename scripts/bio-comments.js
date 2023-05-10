fc969654deff7cd9d00c068e5a839491f19a8968
Andrei Karpenko <7498329@gmail.com>
2023-05-09 23:00:13 -0700

    add submit comment feature

--- scripts/bio-comments.js
+++ scripts/bio-comments.js
@@ -60,7 +60,6 @@ function getCommentsAndAppendToDom(url) {
         avatarContainer.appendChild(innerAvatarEmptyDiv)
         avatarContainer.appendChild(commentListParagraph)
         
-
         commentList.appendChild(avatar)
         commentList.appendChild(avatarContainer)
 
@@ -77,16 +76,53 @@ const submitComment = document.getElementById('submit-comment');
 
 submitComment.addEventListener("click", sendComment);
 
-const newUser = {
-  "name": "Nigel",
-  "comment": "What a cool site"
-}
-
 function sendComment() {
+  const nameValue = document.querySelector('#name').value;
+  const commentValue = document.querySelector('#comment').value;
 
-  axios.post(url + `comments?api_key=1b0f1b14-dce0-4dea-bc62-fcefbb95e821`, newUser).then((response) => {
-      console.log(`bio-comments.js - line: 95 ->> response`, response)
+  const newComment = {
+    name: nameValue,
+    comment: commentValue
+  }
+
+
+  axios.post(url + `comments?api_key=${apiKey}`, newComment).then((response) => {
+    console.log(`bio-comments.js - line: 95 ->> response`, response)
+
+    const commentList = document.createElement('div');
+    const avatar = document.createElement('div');
+    const avatarContainer = document.createElement('div');
+    const innerAvatarEmptyDiv = document.createElement('div')
+    const commentListParagraph = document.createElement('p')
+    const commentListParagraphName = document.createElement('p')
+    const commentListParagraphDate = document.createElement('p')
+    const ticketsSeparator = document.createElement('hr');
+  
+    ticketsSeparator.classList.add('tickets__separator');
+    commentList.classList.add('comment-list');
+    avatar.classList.add('avatar');
+    avatarContainer.classList.add('avatar__container');
+    commentListParagraph.classList.add('comment-list__paragraph');
+    commentListParagraphName.classList.add('comment-list__paragraph', 'is--bolded')
+    commentListParagraphDate.classList.add('comment-list__paragraph', 'is--grey')
+  
+    commentListParagraph.innerText = response.data.comment
+    commentListParagraphName.innerText = response.data.name
+    commentListParagraphDate.innerText = new Date(response.data.timestamp).toLocaleDateString("en-US")
+  
+    innerAvatarEmptyDiv.append(commentListParagraphName, commentListParagraphDate)
+    avatarContainer.appendChild(innerAvatarEmptyDiv)
+    avatarContainer.appendChild(commentListParagraph)
+    
+    commentList.appendChild(avatar)
+    commentList.appendChild(avatarContainer)
+  
+    document.querySelector('.comments-default').prepend(ticketsSeparator);
+    document.querySelector('.comments-default').prepend(commentList);
+
   })
+
+ 
 
 }
 
