6e3d0ce46f8717fdcae74f97335bfd926b1679db
Andrei Karpenko <7498329@gmail.com>
2023-05-13 00:26:43 -0700

    refactor code

--- scripts/bio-comments.js
+++ scripts/bio-comments.js
@@ -2,253 +2,301 @@
 
 const storedApiKey = sessionStorage.getItem('api_key')
 
-let apiKey;
-const url = 'https://project-1-api.herokuapp.com/';
+let apiKey
+const url = 'https://project-1-api.herokuapp.com/'
 
 // Function to retrieve API key from server
 function registerWithApi(url) {
-  axios.get(url + 'register')
-    .then(response => {
+  axios
+    .get(url + 'register')
+    .then((response) => {
       // Save API key in global variable
-      apiKey = response.data.api_key;
+      apiKey = response.data.api_key
       // Save key to localstorage for later use
-      sessionStorage.setItem('api_key', apiKey);
+      sessionStorage.setItem('api_key', apiKey)
 
       // Save API key in global variable
-      getCommentsAndAppendToDom(url);
+      getCommentsAndAppendToDom(url)
     })
-    .catch(error => {
-      console.error('Registration error:', error);
-    });
+    .catch((error) => {
+      console.error('Registration error:', error)
+    })
 }
 
 // Get API key from server
-  
+
 if (storedApiKey) {
   apiKey = storedApiKey
-  getCommentsAndAppendToDom(url);
-
+  getCommentsAndAppendToDom(url)
 } else {
-  registerWithApi(url);
+  registerWithApi(url)
 }
 
 // Function to retrieve comments from API and add them to the page
 function getCommentsAndAppendToDom(url) {
   // Add API key to query params
   const params = {
-    api_key: apiKey
-  };
+    api_key: apiKey,
+  }
 
-  axios.get(url + 'comments', { params: params }).then(response => {
+  axios
+    .get(url + 'comments', { params: params })
+    .then((response) => {
       // Get comments from API response
-      const comments = response.data;
+      const comments = response.data
 
       // Add comments to the page
-      comments.sort((a , b) => b.timestamp - a.timestamp).forEach(comment => {
-        const commentList = document.createElement('div');
-        const avatar = document.createElement('div');
-        const avatarContainer = document.createElement('div');
-        const innerAvatarEmptyDiv = document.createElement('div')
-        const commentListParagraph = document.createElement('p')
-        const commentListParagraphName = document.createElement('p')
-        const commentListParagraphDate = document.createElement('p')
-        const ticketsSeparator = document.createElement('hr');
+      comments
+        .sort((a, b) => b.timestamp - a.timestamp)
+        .forEach((comment) => {
+          const commentList = document.createElement('div')
+          const avatar = document.createElement('div')
+          const avatarContainer = document.createElement('div')
+          const innerAvatarEmptyDiv = document.createElement('div')
+          const commentListParagraph = document.createElement('p')
+          const commentListParagraphName = document.createElement('p')
+          const commentListParagraphDate = document.createElement('p')
+          const ticketsSeparator = document.createElement('hr')
 
-        const commentsButtons = document.createElement('div');
-        const commentButtonLike = document.createElement('button');
-        const commentButtonLikeCount = document.createElement('p');
-        const commentButtonDelete = document.createElement('button');
+          const commentsButtons = document.createElement('div')
+          const commentButtonLike = document.createElement('button')
+          const commentButtonLikeCount = document.createElement('p')
+          const commentButtonDelete = document.createElement('button')
 
-        ticketsSeparator.classList.add('tickets__separator');
-        commentList.classList.add('comment-list');
-        avatar.classList.add('avatar');
-        avatarContainer.classList.add('avatar__container');
-        commentListParagraph.classList.add('comment-list__paragraph');
-        commentListParagraphName.classList.add('comment-list__paragraph', 'is--bolded')
-        commentListParagraphDate.classList.add('comment-list__paragraph', 'is--grey')
-        commentButtonLikeCount.classList.add('comment-list__paragraph')
-        commentButtonLike.classList.add('comment-list__button')
-        commentButtonDelete.classList.add('comment-list__button')
-        
-        commentList.setAttribute('id', `comment-${comment.id}`)
-        commentButtonLike.setAttribute('data-id', `${comment.id}`)
-        commentButtonLikeCount.setAttribute('id', `count-${comment.id}`)
+          ticketsSeparator.classList.add('tickets__separator')
+          commentList.classList.add('comment-list')
+          avatar.classList.add('avatar')
+          avatarContainer.classList.add('avatar__container')
+          commentListParagraph.classList.add('comment-list__paragraph')
+          commentListParagraphName.classList.add(
+            'comment-list__paragraph',
+            'is--bolded'
+          )
+          commentListParagraphDate.classList.add(
+            'comment-list__paragraph',
+            'is--grey'
+          )
+          commentButtonLikeCount.classList.add('comment-list__paragraph')
+          commentButtonLike.classList.add('comment-list__button')
+          commentButtonDelete.classList.add('comment-list__button')
 
-        commentButtonLike.addEventListener('click', handleLike.bind(null, comment.id))
-        commentButtonDelete.addEventListener('click', handleDelete.bind(null, comment.id))
+          commentList.setAttribute('id', `comment-${comment.id}`)
+          commentButtonLike.setAttribute('data-id', `${comment.id}`)
+          commentButtonLikeCount.setAttribute('id', `count-${comment.id}`)
 
-        commentListParagraph.innerText = comment.comment
-        commentListParagraphName.innerText = comment.name
-        commentListParagraphDate.innerText = new Date(comment.timestamp).toLocaleDateString("en-GB", {
+          commentButtonLike.addEventListener(
+            'click',
+            handleLike.bind(null, comment.id)
+          )
+          commentButtonDelete.addEventListener(
+            'click',
+            handleDelete.bind(null, comment.id)
+          )
+
+          commentListParagraph.innerText = comment.comment
+          commentListParagraphName.innerText = comment.name
+          commentListParagraphDate.innerText = new Date(
+            comment.timestamp
+          ).toLocaleDateString('en-GB', {
             year: 'numeric',
             month: '2-digit',
-            day: '2-digit'
-        });
+            day: '2-digit',
+          })
 
-        commentButtonLikeCount.innerHTML = comment.likes;
+          commentButtonLikeCount.innerHTML = comment.likes
 
-        commentButtonLike.innerHTML = `
+          commentButtonLike.innerHTML = `
         <img src="../assets/icons/like.png" alt="Like" class="comments__buttons--button">
         `
-        commentButtonDelete.innerHTML = `
+          commentButtonDelete.innerHTML = `
         <img src="../assets/icons/delete.png" alt="Delete" class="comments__buttons--button">
         `
-        commentsButtons.append(commentButtonLike, commentButtonLikeCount, commentButtonDelete);
-        
-        innerAvatarEmptyDiv.append(commentListParagraphName, commentListParagraphDate)
-        avatarContainer.appendChild(innerAvatarEmptyDiv)
-        avatarContainer.appendChild(commentListParagraph)
-        avatarContainer.appendChild(commentsButtons)
-        
-        commentList.appendChild(avatar)
-        commentList.appendChild(avatarContainer)
+          commentsButtons.append(
+            commentButtonLike,
+            commentButtonLikeCount,
+            commentButtonDelete
+          )
 
-        document.querySelector('.comments-default').appendChild(commentList);
-        document.querySelector('.comments-default').appendChild(ticketsSeparator);
-      });
+          innerAvatarEmptyDiv.append(
+            commentListParagraphName,
+            commentListParagraphDate
+          )
+          avatarContainer.appendChild(innerAvatarEmptyDiv)
+          avatarContainer.appendChild(commentListParagraph)
+          avatarContainer.appendChild(commentsButtons)
+
+          commentList.appendChild(avatar)
+          commentList.appendChild(avatarContainer)
+
+          document.querySelector('.comments-default').appendChild(commentList)
+          document
+            .querySelector('.comments-default')
+            .appendChild(ticketsSeparator)
+        })
     })
-    .catch(error => {
-      console.error('Error retrieving comments:', error);
-    });
+    .catch((error) => {
+      console.error('Error retrieving comments:', error)
+    })
 }
 
-const submitComment = document.getElementById('submit-comment');
+const submitComment = document.getElementById('submit-comment')
 
-submitComment.addEventListener("click", sendComment);
+submitComment.addEventListener('click', sendComment)
 
 function sendComment() {
-  const nameValue = document.querySelector('#name').value;
-  const commentValue = document.querySelector('#comment').value;
+  const nameValue = document.querySelector('#name').value
+  const commentValue = document.querySelector('#comment').value
 
   const newComment = {
     name: nameValue,
-    comment: commentValue
+    comment: commentValue,
   }
 
-  if (commentValue == "") {
-    document.querySelector('#comment').style.borderColor = "#D22D2D";
-    return;
-  }
-  else {
-    document.querySelector('#comment').style.borderColor = "#E1E1E1";
-  }
-
-  if (nameValue == "") {
-    document.querySelector('#name').style.borderColor = "#D22D2D";
-    return;
-  }
-  else {
-    document.querySelector('#name').style.borderColor = "#E1E1E1";
+  if (commentValue == '') {
+    document.querySelector('#comment').style.borderColor = '#D22D2D'
+    return
+  } else {
+    document.querySelector('#comment').style.borderColor = '#E1E1E1'
   }
 
-  axios.post(url + `comments?api_key=${apiKey}`, newComment).then((response) => {
-    console.log(`bio-comments.js - line: 95 ->> response`, response)
+  if (nameValue == '') {
+    document.querySelector('#name').style.borderColor = '#D22D2D'
+    return
+  } else {
+    document.querySelector('#name').style.borderColor = '#E1E1E1'
+  }
 
-    const commentList = document.createElement('div');
-    const avatar = document.createElement('div');
-    const avatarContainer = document.createElement('div');
-    const innerAvatarEmptyDiv = document.createElement('div')
-    const commentListParagraph = document.createElement('p')
-    const commentListParagraphName = document.createElement('p')
-    const commentListParagraphDate = document.createElement('p')
-    const ticketsSeparator = document.createElement('hr');
+  axios
+    .post(url + `comments?api_key=${apiKey}`, newComment)
+    .then((response) => {
+      console.log(`bio-comments.js - line: 95 ->> response`, response)
 
+      const commentList = document.createElement('div')
+      const avatar = document.createElement('div')
+      const avatarContainer = document.createElement('div')
+      const innerAvatarEmptyDiv = document.createElement('div')
+      const commentListParagraph = document.createElement('p')
+      const commentListParagraphName = document.createElement('p')
+      const commentListParagraphDate = document.createElement('p')
+      const ticketsSeparator = document.createElement('hr')
 
-    const commentsButtons = document.createElement('div');
-    const commentButtonLike = document.createElement('button');
-    const commentButtonDelete = document.createElement('button');
-    const commentButtonLikeCount = document.createElement('p');
+      const commentsButtons = document.createElement('div')
+      const commentButtonLike = document.createElement('button')
+      const commentButtonDelete = document.createElement('button')
+      const commentButtonLikeCount = document.createElement('p')
 
-  
-    ticketsSeparator.classList.add('tickets__separator');
-    commentList.classList.add('comment-list');
-    avatar.classList.add('avatar');
-    avatarContainer.classList.add('avatar__container');
-    commentListParagraph.classList.add('comment-list__paragraph');
-    commentListParagraphName.classList.add('comment-list__paragraph', 'is--bolded')
-    commentListParagraphDate.classList.add('comment-list__paragraph', 'is--grey')
-    commentButtonLikeCount.classList.add('comment-list__paragraph')
-    commentButtonLike.classList.add('comment-list__button')
-    commentButtonDelete.classList.add('comment-list__button')
-  
-    commentList.setAttribute('id', `comment-${response.data.id}`)
-    commentButtonLike.setAttribute('data-id', `${response.data.id}`)
-    commentButtonLikeCount.setAttribute('id', `count-${response.data.id}`)
+      ticketsSeparator.classList.add('tickets__separator')
+      commentList.classList.add('comment-list')
+      avatar.classList.add('avatar')
+      avatarContainer.classList.add('avatar__container')
+      commentListParagraph.classList.add('comment-list__paragraph')
+      commentListParagraphName.classList.add(
+        'comment-list__paragraph',
+        'is--bolded'
+      )
+      commentListParagraphDate.classList.add(
+        'comment-list__paragraph',
+        'is--grey'
+      )
+      commentButtonLikeCount.classList.add('comment-list__paragraph')
+      commentButtonLike.classList.add('comment-list__button')
+      commentButtonDelete.classList.add('comment-list__button')
 
-    commentButtonLike.addEventListener('click', handleLike.bind(null, response.data.id))
-    commentButtonDelete.addEventListener('click', handleDelete.bind(null, response.data.id))
+      commentList.setAttribute('id', `comment-${response.data.id}`)
+      commentButtonLike.setAttribute('data-id', `${response.data.id}`)
+      commentButtonLikeCount.setAttribute('id', `count-${response.data.id}`)
 
-    commentListParagraph.innerText = response.data.comment
-    commentListParagraphName.innerText = response.data.name
-    commentListParagraphDate.innerText = new Date(response.data.timestamp).toLocaleDateString("en-GB", {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit'
-    })
+      commentButtonLike.addEventListener(
+        'click',
+        handleLike.bind(null, response.data.id)
+      )
+      commentButtonDelete.addEventListener(
+        'click',
+        handleDelete.bind(null, response.data.id)
+      )
 
-    commentButtonLikeCount.innerHTML = response.data.likes;
-    commentButtonLike.innerHTML = `
+      commentListParagraph.innerText = response.data.comment
+      commentListParagraphName.innerText = response.data.name
+      commentListParagraphDate.innerText = new Date(
+        response.data.timestamp
+      ).toLocaleDateString('en-GB', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+
+      commentButtonLikeCount.innerHTML = response.data.likes
+      commentButtonLike.innerHTML = `
     <img src="../assets/icons/like.png" alt="Like" class="comments__buttons--button">
     `
-    commentButtonDelete.innerHTML = `
+      commentButtonDelete.innerHTML = `
     <img src="../assets/icons/delete.png" alt="Delete" class="comments__buttons--button">
     `
-    commentsButtons.append(commentButtonLike, commentButtonLikeCount, commentButtonDelete);
-    innerAvatarEmptyDiv.append(commentListParagraphName, commentListParagraphDate)
-    avatarContainer.appendChild(innerAvatarEmptyDiv)
-    avatarContainer.appendChild(commentListParagraph)
-    avatarContainer.appendChild(commentsButtons)
-    
-    commentList.appendChild(avatar)
-    commentList.appendChild(avatarContainer)
-  
-    document.querySelector('.comments-default').prepend(ticketsSeparator);
-    document.querySelector('.comments-default').prepend(commentList);
+      commentsButtons.append(
+        commentButtonLike,
+        commentButtonLikeCount,
+        commentButtonDelete
+      )
+      innerAvatarEmptyDiv.append(
+        commentListParagraphName,
+        commentListParagraphDate
+      )
+      avatarContainer.appendChild(innerAvatarEmptyDiv)
+      avatarContainer.appendChild(commentListParagraph)
+      avatarContainer.appendChild(commentsButtons)
 
-    // clear form
-    document.querySelector('#name').value = "";
-    document.querySelector('#comment').value = "";
-  })}
+      commentList.appendChild(avatar)
+      commentList.appendChild(avatarContainer)
 
+      document.querySelector('.comments-default').prepend(ticketsSeparator)
+      document.querySelector('.comments-default').prepend(commentList)
+
+      // clear form
+      document.querySelector('#name').value = ''
+      document.querySelector('#comment').value = ''
+    })
+}
 
 // DIVING DEEPER
 
-function handleLike(id, event) { //function for likes
+function handleLike(id, event) {
+  //function for likes
   event.stopPropagation() // stop event from triggering other on click events
 
   const likeCountById = document.getElementById(`count-${id}`)
 
-  axios.put(url + `comments/${id}/like?api_key=${apiKey}`)
-  .then((response) => {
-    if (response.status === 200) {
-      likeCountById.innerText = response.data.likes;
-    }
-  }).catch((error) =>{
-    console.error(`Like error`, error);
-  })
+  axios
+    .put(url + `comments/${id}/like?api_key=${apiKey}`)
+    .then((response) => {
+      if (response.status === 200) {
+        likeCountById.innerText = response.data.likes
+      }
+    })
+    .catch((error) => {
+      console.error(`Like error`, error)
+    })
 }
 
-// NOT FINISHED
+function handleDelete(id, event) {
+  // function for deleting comments
+  event.stopPropagation() // stop event from triggering other on click events
 
-function handleDelete(id, event) { // function for deleting comments
-  event.stopPropagation(); // stop event from triggering other on click events
+  const commentElement = document.getElementById(`comment-${id}`)
+  const commentSeparator = commentElement.nextElementSibling
 
-  const commentElement = document.getElementById(`comment-${id}`);
-  const commentSeparator = commentElement.nextElementSibling;
+  const result = confirm('You sure you want to delete this comment?')
 
-  const result = confirm("You sure you want to delete this comment?");
-  
   if (result) {
-    axios.delete(url + `comments/${id}/?api_key=${apiKey}`)
+    axios
+      .delete(url + `comments/${id}/?api_key=${apiKey}`)
       .then((response) => {
-
         if (response.status === 200) {
-          commentElement.remove();
-          commentSeparator.remove();
+          commentElement.remove()
+          commentSeparator.remove()
         }
       })
       .catch((error) => {
-        console.error(`Delete error`, error);
-      });
+        console.error(`Delete error`, error)
+      })
   }
 }
--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -2,141 +2,153 @@
 
 const storedApiKey = sessionStorage.getItem('api_key')
 
-let apiKey;
-const url = 'https://project-1-api.herokuapp.com/';
+let apiKey
+const url = 'https://project-1-api.herokuapp.com/'
 
 function registerWithApi(url) {
-  axios.get(url + 'register')
-    .then(response => {
+  axios
+    .get(url + 'register')
+    .then((response) => {
       // Save API key in global variable
-      apiKey = response.data.api_key;
+      apiKey = response.data.api_key
       // Save key to localstorage for later use
-      sessionStorage.setItem('api_key', apiKey);
-      
-      // get shows, save them to a variable
-      getShowDates( url );
-      // Call the renderShows function with the showsData array as an argument
-      ;
-    })
-    .catch(error => {
-      console.error('Registration error:', error);
-    });
-}
+      sessionStorage.setItem('api_key', apiKey)
 
+      // get shows, save them to a variable
+      getShowDates(url)
+      // Call the renderShows function with the showsData array as an argument
+    })
+    .catch((error) => {
+      console.error('Registration error:', error)
+    })
+}
 
 if (storedApiKey) {
   apiKey = storedApiKey
   // get shows, save them to a variable
-  getShowDates( url );
+  getShowDates(url)
   // Call the renderShows function with the showsData array as an argument
-  
 } else {
   // Get API key from server
-  registerWithApi(url);
+  registerWithApi(url)
 }
 
 function getShowDates(url) {
-  
   const params = {
-    api_key: apiKey
-  };
+    api_key: apiKey,
+  }
 
-  axios.get(url + 'showDates', { params: params }).then(response => {
-
+  axios.get(url + 'showDates', { params: params }).then((response) => {
     if (response.status === 200) {
       // Call the renderShows function with the showsData array as an argument
-      renderShows(response.data);
-
+      renderShows(response.data)
     } else {
-      renderShows([{
-        date: 'no data',
-        place: 'no data',
-        location: 'no data'
-      }])
+      renderShows([
+        {
+          date: 'no data',
+          place: 'no data',
+          location: 'no data',
+        },
+      ])
     }
-
   })
 }
 
 // Use JavaScript to dynamically create the HTML elements for each show and add them to the DOM
 function renderShows(showsData) {
   // Create a div element to contain all the shows' data
-  showsList = document.querySelector('.tickets__container');
+  showsList = document.querySelector('.tickets__container')
 
   // Iterate over each show's data and create HTML elements for each
-  showsData.forEach(show => {
+  showsData.forEach((show) => {
     // Create a div element for each show
-    const showElement = document.createElement('div');
-    showElement.classList.add('tickets__row');
-    
+    const showElement = document.createElement('div')
+    showElement.classList.add('tickets__row')
+
     // Create a p element for the date title
-    const dataTitle = document.createElement('p');
-    dataTitle.classList.add('tickets__header--date', 'tickets__header-cell', 'desktop-hidden');
-    dataTitle.textContent = 'DATE';
-    showElement.appendChild(dataTitle);
+    const dataTitle = document.createElement('p')
+    dataTitle.classList.add(
+      'tickets__header--date',
+      'tickets__header-cell',
+      'desktop-hidden'
+    )
+    dataTitle.textContent = 'DATE'
+    showElement.appendChild(dataTitle)
 
     // Create a p element for the date value
-    const dateElement = document.createElement('p');
-    dateElement.classList.add('tickets__date', 'tickets__cell--bolder', 'tickets__cell');
-    const formattedDate = new Date(show.date).toLocaleDateString("en-US", {
-    weekday: 'short',
-    month: 'short',
-    day: 'numeric',
-    year: 'numeric'
-    });
-    dateElement.textContent = formattedDate;
-    showElement.appendChild(dateElement);
-
+    const dateElement = document.createElement('p')
+    dateElement.classList.add(
+      'tickets__date',
+      'tickets__cell--bolder',
+      'tickets__cell'
+    )
+    const formattedDate = new Date(show.date).toLocaleDateString('en-US', {
+      weekday: 'short',
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+    })
+    dateElement.textContent = formattedDate
+    showElement.appendChild(dateElement)
 
     // Create a p element for the venue title
-    const venueTitle = document.createElement('p');
-    venueTitle.classList.add('tickets__header--venue', 'tickets__header-cell', 'desktop-hidden');
-    venueTitle.textContent = 'VENUE';
-    showElement.appendChild(venueTitle);
+    const venueTitle = document.createElement('p')
+    venueTitle.classList.add(
+      'tickets__header--venue',
+      'tickets__header-cell',
+      'desktop-hidden'
+    )
+    venueTitle.textContent = 'VENUE'
+    showElement.appendChild(venueTitle)
 
     // Create a p element for the venue value
-    const venueElement = document.createElement('p');
-    venueElement.classList.add('tickets__venue', 'tickets__cell');
-    venueElement.textContent = show.place;
-    showElement.appendChild(venueElement);
+    const venueElement = document.createElement('p')
+    venueElement.classList.add('tickets__venue', 'tickets__cell')
+    venueElement.textContent = show.place
+    showElement.appendChild(venueElement)
 
     // Create a p element for the location title
-    const locationTitle = document.createElement('p');
-    locationTitle.classList.add('tickets__header--venue', 'tickets__header-cell', 'desktop-hidden');
-    locationTitle.textContent = 'LOCATION';
-    showElement.appendChild(locationTitle);
+    const locationTitle = document.createElement('p')
+    locationTitle.classList.add(
+      'tickets__header--venue',
+      'tickets__header-cell',
+      'desktop-hidden'
+    )
+    locationTitle.textContent = 'LOCATION'
+    showElement.appendChild(locationTitle)
 
     // Create a p element for the location value
-    const locationElement = document.createElement('p');
-    locationElement.classList.add('tickets__location', 'tickets__cell');
-    locationElement.textContent = show.location;
-    showElement.appendChild(locationElement);
+    const locationElement = document.createElement('p')
+    locationElement.classList.add('tickets__location', 'tickets__cell')
+    locationElement.textContent = show.location
+    showElement.appendChild(locationElement)
 
     // Create a button element for buying tickets
-    const buyButton = document.createElement('button');
-    buyButton.classList.add('tickets__buy-button');
-    buyButton.textContent = 'Buy Tickets';
-    showElement.appendChild(buyButton);
+    const buyButton = document.createElement('button')
+    buyButton.classList.add('tickets__buy-button')
+    buyButton.textContent = 'Buy Tickets'
+    showElement.appendChild(buyButton)
 
-    showsList.appendChild(showElement);
-    showElement.addEventListener('click', (event) => { //add background-color to the current row
-      document.querySelectorAll('.tickets__row').forEach(row => {
-        row.style.backgroundColor = 'white';
-      }) 
-      event.currentTarget.style.backgroundColor = '#E1E1E1';
+    showsList.appendChild(showElement)
+    showElement.addEventListener('click', (event) => {
+      //add background-color to the current row
+      document.querySelectorAll('.tickets__row').forEach((row) => {
+        row.style.backgroundColor = 'white'
+      })
+      event.currentTarget.style.backgroundColor = '#E1E1E1'
     })
 
     // Create a divider line element
-    const dividerLine = document.createElement('hr');
-    dividerLine.classList.add('tickets__separator');
-    showsList.appendChild(dividerLine);
-  });
+    const dividerLine = document.createElement('hr')
+    dividerLine.classList.add('tickets__separator')
+    showsList.appendChild(dividerLine)
+  })
 
   function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)
   }
-  
-  const main = document.querySelector('.main');
+
+  const main = document.querySelector('.main')
 
   insertAfter(main, showsList)
 }
